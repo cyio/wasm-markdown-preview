4784aded3a95f653dd57c58c8fcbf950e9fb1650
gongyi01 <gongyi01@baidu.com>
2023-11-10 16:56:23 +0800

    work demo

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// src/lib.rs
+
+use wasm_bindgen::prelude::*;
+use pulldown_cmark::{html, Parser};
+
+#[wasm_bindgen]
+pub fn parse_markdown(input: &str) -> String {
+    let parser = Parser::new(input);
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+    html_output
+}
