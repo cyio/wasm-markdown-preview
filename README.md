9f557971751d1cdd8a0f9dcacf278fce085de8a0
gongyi01 <gongyi01@baidu.com>
2023-11-10 16:56:23 +0800

    code comment & readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# wasm-markdown-preview
+
+[使用 rust & wasm 实现在浏览器中解析 markdown](https://chat.openai.com/share/7e9ee5d8-b686-4c00-9f32-110d52d6e564)
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
-// src/lib.rs
-
 use wasm_bindgen::prelude::*;
-use pulldown_cmark::{html, Parser};
+use pulldown_cmark::{html, Parser}; // parser 负责解析 Markdown 文本，将其转换为内部表示的 Markdown 元素，而 html 模块则负责将这些 Markdown 元素转换为 HTML 字符串
 
-#[wasm_bindgen]
+#[wasm_bindgen] // 标记导出给 wasm
 pub fn parse_markdown(input: &str) -> String {
     let parser = Parser::new(input);
-    let mut html_output = String::new();
-    html::push_html(&mut html_output, parser);
-    html_output
+    let mut html_output = String::new(); // 可变 string
+    html::push_html(&mut html_output, parser); // 调用 html::push_html 函数
+    html_output // 返回
 }
